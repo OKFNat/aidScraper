bfd7517f650090ce6d460f10c23a3f5d4fa4207a
Stefan Kasberger <mail@stefankasberger.at>
2016-04-03 17:26:44 +0200

    extended scraper; created README.md, add LICENSE

--- README.md
+++ README.md
@@ -1,2 +1,123 @@
-# Scrape_ADC
+Austrian aid scraper
+==============================
+The scraper extracts information from the EU arms export reports between 2005 and 2013, which is very hard to read for machines. The automatically extracted information is then stored in different data structures (network, country specific) and file formats (CSV, JSON), which are relevant for the next steps, like network analysis, visualization and statistical analysis.
+
 Scrapes data for all development cooperation projects on Austrian Development Cooperation website
+
+- Team: Gute Taten für gute Daten Project (Open Knowledge Austria)
+- Status: Production
+- Documentation: English
+- License: [MIT License](http://opensource.org/licenses/MIT)
+- Website: [Gute Taten für gute Daten project](http://okfn.at/gutedaten/) 
+
+**Used software**
+The sourcecode is written in Python 2. It was created with use of [iPython](http://ipython.org/), [BeautifulSoup4](http://www.crummy.com/software/BeautifulSoup/) and [urllib2](https://docs.python.org/2/library/urllib2.html).
+
+**Copyright**
+
+All content is openly licensed under the [Creative Commons Attribution 4.0](http://creativecommons.org/licenses/by/4.0/) license, unless otherwisely stated.
+
+All sourcecode is openly licensed under the [MIT license](http://opensource.org/licenses/MIT), unless otherwisely stated.
+
+## SCRAPER
+
+**Description**
+
+The scraper fetches the html passed in as urls from a csv file and stores them locally. The html is then parsed with BeautifulSoup4. Every table between the requested start country and end country is parsed out row by row, cell by cell and stored into a JSON structure with importing countries -> exporting countries -> arms classes -> data. The data structure is then  used to create nodes and edges files as JSON and CSV. This can also be used to extract country specific data to understand imports and exports from a country's perspective.
+
+**Run scraper**
+Go into the root folder of this repository and execute following commands in your terminal:
+```
+cd code
+python aid-scraper.py
+```
+
+## DATA INPUT
+The original data is from the project list of the austrian development agency (ADA) [published on their website](http://www.entwicklung.at/nc/zahlen-daten-und-fakten/projektliste/?tx_sysfirecdlist_pi1[test]=test&tx_sysfirecdlist_pi1[mode]=1&tx_sysfirecdlist_pi1[sort]=uid%3A1&tx_sysfirecdlist_pi1[pointer]=). The data consists of all contracts approved since January 1st of 2010 in chronological order. The date of the last update can be found on the first table page as "Datum der letzten Aktualisierung".
+
+### The Tables
+The tables are the basic data, where most of the data is parsed out. The data is published in the following structure (e. g. first project in the list).
+
+
+| Vertragsnummer | Vertragstitel | Land/Region | OEZA/ADA-Vertragssumme | Vertragspartner |
+|----------------|---------------|-------------|------------------------|-----------------|
+| 2325-02/2016 | Programm zum Schutz der MenschenrechtsverteidigerInnen in der westlichen Region Guatemalas | Guatemala | EUR 64.300,00 | HORIZONT3000 - Österreichische Organisation für Entwicklungszusammena |
+
+**Attributes**
+- Vertragsnummer: contract number of project.
+- Vertragstitel: title of project.
+- Land/Region: country or region, where project takes place at.
+- OEZA/ADA-Vertragssumme: amount of money granted by contract.
+- Vertragspartner: partner(s) in the project.
+
+
+### The project pages
+When you click on the contract titel in a table you get to the project page. It consists of the same data as the table view, except the additional description text (named "Beschreibung").
+
+### Soundness
+- So far, we can not say anything about the data quality (completeness, accurateness, etc.), but there are also so far no reaseons to doubt the quality.
+
+## DATA OUTPUT
+
+**raw html**
+
+The scraper downloads all raw html of each table and each project page.
+
+**aid data JSON**
+
+The parsed data is stored in an easy-to-read JSON file for further usage.
+```
+[
+	{
+		'contract-number': contract number of the project
+		'contract-title': title of the project
+		'country-region': country and/or region, where the project takes place
+		'OEZA-ADA-contract-volume': amount of funding by austrian development agency
+		'contract-partner': partner organisation(s)
+		'description': description text of the project
+		'url': url of the project page
+	},
+]
+```
+
+**aid data csv**
+
+The parsed data is stored in a human-readable CSV file for further usage.
+
+columns (see attribute description above):
+- contract-number
+- contract-title
+- OEZA-ADA-contract-volume
+- contract-partner
+- country-region
+- description
+- url
+
+row: one project each row.
+
+## STRUCTURE
+- [README.md](README.md): Overview of repository
+- [CHANGELOG.md](README.md): Overview of repository
+
+## TODO
+**important**
+- verify the data
+- research: is there a difference between approved funding and paid one?
+
+**optional**
+- create dataset for network analyses: json, csv for gephi and networkX
+- update code to Python3
+- compare data from tables with data from project pages.
+
+**new features**
+- analyze and visualize the data: networkX, maps, Gephi
+- add country namecodes for easier combinating with other data
+
+## CHANGELOG
+### Version 0.2 - 2015-10-29
+**init repo**
+
+
+
+
+
---
+++ code/aid-scraper.py
@@ -0,0 +1,335 @@
+#!/bin/env python2
+# -*- coding: utf-8 -*-
+
+"""
+Fetches, parses and stores the data from the austrian aid projects website since January 1st 2010. 
+"""
+
+import urllib2
+import bs4
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+import json
+import os
+import string
+import time
+
+__author__ = "Christian Goedel, Stefan Kasberger"
+__copyright__ = "Copyright 2015"
+__license__ = "MIT"
+__version__ = "0.2"
+__maintainer__ = "Stefan Kasberger"
+__email__ = "mail@stefankasberger.at"
+__status__ = "Production" # 'Development', 'Production' or 'Prototype'
+
+###    GLOBAL   ###
+
+ROOT_FOLDER = os.path.dirname(os.getcwd())
+FOLDER_RAW_HTML = ROOT_FOLDER + '/data/raw/html/'
+FOLDER_CSV = ROOT_FOLDER + '/data/csv/'
+FOLDER_JSON = ROOT_FOLDER + '/data/json/'
+FILENAME_BASE = 'aid-data'
+BASE_URL = 'http://www.entwicklung.at/nc/zahlen-daten-und-fakten/projektliste/'
+QUERY_URL = BASE_URL + '?tx_sysfirecdlist_pi1[test]=test&tx_sysfirecdlist_pi1[mode]=1&tx_sysfirecdlist_pi1[sort]=uid%3A1&tx_sysfirecdlist_pi1[pointer]='
+DELAY_TIME = 5 # in seconds
+# TS = datetime.now().strftime('%Y-%m-%d-%H-%M')
+TS = '2016-04-03-03-05'
+
+###    FUNCTIONS   ###
+
+def SetupEnvironment():
+	"""Sets up the folder structure and working environment.
+	"""
+	if not os.path.exists(FOLDER_RAW_HTML):
+		os.makedirs(FOLDER_RAW_HTML)
+	if not os.path.exists(FOLDER_JSON):
+		os.makedirs(FOLDER_JSON)
+	if not os.path.exists(FOLDER_CSV):
+		os.makedirs(FOLDER_CSV)
+
+def FetchHtml(url):
+	"""Fetches html url via urllib2.
+	
+	Args:
+		url: url to fetch
+	
+	Returns:
+		html: html string as unicode
+	"""
+	response = urllib2.urlopen(url)
+	html = response.read().decode('utf-8')
+	time.sleep(DELAY_TIME)
+	
+	return html
+
+def FetchHtmlTables(url, folder, pageCounter = 0):
+	"""Fetches each overview page with the table of the projects and saves the html locally.
+	
+	Args:
+		url: string to fetch.
+		folder: directory to save the html files in.
+		pageCounter: counter for the actual number of the page (int).
+	
+	Returns:
+		none
+	"""
+	# the url has a page-counter at the end, so just increase the counter and fetch one page after another
+	rawHtml = FetchHtml(url+str(pageCounter))
+
+	if not os.path.exists(folder+TS):
+		os.makedirs(folder+TS)
+	Save2File(rawHtml, folder+TS+'/'+TS+'_table-'+str(pageCounter)+'.html')
+
+	# checks if there is a "weiter" (more) anchor, cause this means a next page exists
+	isWeiter = re.findall(r'<a href="(.*)" >weiter &gt;</a></div></div>', rawHtml)
+	if isWeiter:
+		pageCounter += 1
+		FetchHtmlTables(url, folder, pageCounter)
+
+def FetchHtmlProjects(aidData, folder):
+	"""Fetches html of the project pages and saves the html locally.
+	
+	Args:
+		aidData: list[] of dict{} of aid projects.
+		folder: directory to save the html files in.
+	
+	Returns:
+		aidData: list[] of dict{} of aid projects.
+	"""
+	if not os.path.exists(folder+TS):
+		os.makedirs(folder+TS)
+	# iterate over every list element to fetch and store the project page
+	for idx, project in enumerate(aidData):
+		html = FetchHtml(project['url'])
+		Save2File(html, folder+TS+'/'+TS+'_project-'+str(idx)+'.html')
+
+	return aidData
+
+def Save2File(data, filename):
+	"""Saves a file locally.
+	
+	Args:
+		data: string to save.
+		filename: name of the file with path.
+	
+	Returns:
+		none
+	"""
+	text_file = open(filename, "w")
+	text_file.write(data.encode('utf-8'))
+	text_file.close()
+
+def ReadFile(filename):
+	"""Reads a file and returns the string.
+	
+	Args:
+		filename: name of the file
+	
+	Returns:
+		string: file as text-string.
+	"""
+	f = open(filename, 'r')
+	string = f.read()
+	
+	return string
+
+def ReadTableFilesInFolder(folder):
+	"""Reads in all table html-files from a folder.
+	
+	Args:
+		folder: folder where the html-files are stored in.
+	
+	Returns:
+		sortedList: list[] of sorted html texts.
+	"""
+	htmlList = []
+	sortedList = []
+
+	for filename in os.listdir(folder):
+		if filename.find(TS+'_table') >= 0:
+			rawHtml = ReadFile(folder+'/'+filename)
+			fileIndex = filename.split('.')[0].split('-')[5]
+			htmlList.append((int(fileIndex), rawHtml))
+	# sort list of duppels after first element (the filename postfix) and store to list[]
+	htmlList = sorted(htmlList, key=lambda htmlList: htmlList[0])
+	for idx, html in htmlList:
+		sortedList.append(html)
+
+	return sortedList
+
+def ReadProjectFilesInFolder(aidData, folder):
+	"""Reads in all table html-files from a folder.
+	
+	Args:
+		aidData: list[] of dict{} of aid projects.
+		folder: folder where the html-files are stored in.
+	
+	Returns:
+		sortedList: list[] of sorted html texts.
+	"""
+	htmlList = []
+	sortedList = []
+
+	for filename in os.listdir(folder):
+		if filename.find(TS+'_project') >= 0:
+			rawHtml = ReadFile(folder+'/'+filename)
+			fileIndex = filename.split('.')[0].split('-')[5]
+			htmlList.append((int(fileIndex), rawHtml))
+	
+	# sort list of duppels after first element (the filename postfix) and store to list[]
+	htmlList = sorted(htmlList, key=lambda htmlList: htmlList[0])
+	for idx, html in htmlList:
+		sortedList.append(html)
+
+	return sortedList
+
+def ParseTables(html):
+	"""Parses each row of the html table.
+	
+	Args:
+		html: list[] of duppels (filename, html).
+	
+	Returns:
+		aidData: list[] of dict{} of aid projects.
+			'contract-number': contract number.
+			'contract-title': title of the project.
+			'country-region': country and/or region, where the project takes place.
+			'OEZA-ADA-contract-volume': amount of funding.
+			'contract-partner': partner organisation(s).
+			'url': url of the project page.
+	"""
+	aidData = []
+	for page in html:
+		soup = BeautifulSoup(page)
+		table = soup.find_all('table')[1]
+		for row in table.find_all('tr')[1:]:
+			project = {}
+			tds = row.find_all('td')
+			project['contract-title'] = tds[1].div.text
+			project['url'] = tds[1].div.a['href']
+			project['country-region'] = tds[2].div.text
+			project['OEZA-ADA-contract-volume'] = tds[3].div.text
+			project['contract-partner'] = tds[4].div.text
+			project['contract-number'] = tds[0].div.text
+			aidData.append(project)
+	
+	return aidData
+
+def ParseProjects(aidData, htmlProjects):
+	"""Parses the description of every project page.
+	
+	Args:
+		aidData: list[] of dict{} of aid projects.
+		htmlProjects: list[] of html texts.
+	
+	Returns:
+		aidData: list[] of dict{} of aid projects.
+			'contract-number': contract number.
+			'contract-title': title of the project.
+			'country-region': country and/or region, where the project takes place.
+			'OEZA-ADA-contract-volume': amount of funding.
+			'contract-partner': partner organisation(s).
+			'description': description text of the project.
+			'url': url of the project page.
+	"""
+	for idx, html in enumerate(htmlProjects):
+		soup = BeautifulSoup(html)
+		aidData[int(idx)]['description'] = soup.find_all('table')[0].find_all('tr')[5].find_all('div')[1].text
+	
+	return aidData
+
+def SaveAidData(aidData, filename):
+	"""Saves the aid data as JSON file.
+	
+	Args:
+		aidData: list[] of dict{} of aid projects.
+		filename: filepath of the JSON file. 
+	
+	Returns:
+		none
+	"""
+	Save2File(json.dumps(aidData, indent=2, ensure_ascii=False), filename)
+
+def OpenAidData(filename):
+	"""Opens the aid data JSON file.
+	
+	Args:
+		filename: filepath of the JSON file.
+	
+	Returns:
+		none
+	"""
+	aidData = json.loads(ReadFile(filename))
+	
+	return aidData
+
+def Save2CSV(data, filename):
+    """Exports the aid data into a csv file.
+    
+    Args:
+        data: list[] of dict{} of aid projects.
+        filename: filepath.
+    
+    Returns:
+        none
+    """
+    csvString = '"contract-number";"contract-title";"OEZA-ADA-contract-volume";"contract-partner";"country-region";"description";"url"\n'
+    # iterate over each project
+    for project in data:
+		number = '""'
+		title = '""'
+		funding = '""'
+		partner = '""'
+		region = '""'
+		description = '""'
+		url = '""'
+		# read out each attribute
+		for elem in project.keys():
+			if elem == 'contract-number':
+				number = '"'+project[elem]+'"'
+			if elem == 'contract-title':
+				title = '"'+project[elem]+'"'
+			if elem == 'OEZA-ADA-contract-volume':
+				funding = '"'+project[elem]+'"'
+			if elem == 'contract-partner':
+				partner = '"'+project[elem]+'"'
+			if elem == 'country-region':
+				region = '"'+project[elem]+'"'
+			if elem == 'description':
+				description = '"'+project[elem]+'"'
+			if elem == 'url':
+				url = '"'+project[elem]+'"'
+			
+		csvString += number+';'+title+';'+funding+';'+partner+';'+region+';'+description+';'+url+'\n'
+
+    Save2File(csvString, filename)
+
+if __name__ == "__main__":
+
+	# setup
+	print 'start:',TS
+	aidData = []
+	SetupEnvironment()
+	DOWNLOAD_FILES = False
+	PARSE_FILES = False
+	STRUCTURE_DATA = True
+
+	if DOWNLOAD_FILES:
+		FetchHtmlTables(QUERY_URL, FOLDER_RAW_HTML) # html as string
+		htmlTables = ReadTableFilesInFolder(FOLDER_RAW_HTML+TS) # html as string
+		aidData = ParseTables(htmlTables)
+		SaveAidData(aidData, FOLDER_JSON+'aid-data_'+TS+'.json')
+		FetchHtmlProjects(aidData, FOLDER_RAW_HTML)
+	
+	if PARSE_FILES:
+		aidData = OpenAidData(FOLDER_JSON+'aid-data_'+TS+'.json')
+		htmlProjects = ReadProjectFilesInFolder(aidData, FOLDER_RAW_HTML+TS) # html as string
+		aidData = ParseProjects(aidData, htmlProjects)
+		SaveAidData(aidData, FOLDER_JSON+'aid-data_'+TS+'.json')
+
+	if STRUCTURE_DATA:
+		aidData = OpenAidData(FOLDER_JSON+'aid-data_'+TS+'.json')
+		Save2CSV(aidData, FOLDER_CSV+'aid-data_'+TS+'.csv')
+    
+	print 'runtime:', (datetime.now() - datetime.strptime(TS, '%Y-%m-%d-%H-%M'))
